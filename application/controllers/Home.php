3b8ed84afe174cc20b6224e526e210f7b19adce4
QuynhLien <voxuanquynhlien@gmail.com>
2019-08-10 01:56:10 +0700

    contructs

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends CI_Controller
 {
     function __construct()
     {
-        parent::__construct();
+        // parent::__construct();
 
         $this->load->model('Home_model');
     }
