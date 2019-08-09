0c2a5edf771c448ceab7493909f347caa5c03db8
QuynhLien <voxuanquynhlien@gmail.com>
2019-08-10 01:54:07 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://milkteatocotoco.herokuapp.com/';//'http://localhost/tocotoco/';
+$config['base_url'] = 'https://tocotocomilktea.herokuapp.com/';//'http://localhost/tocotoco/';
 
 /*
 |--------------------------------------------------------------------------
