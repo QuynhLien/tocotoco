9dd4d890f8e7f4dfbb5323b024848571d4d2d437
QuynhLien <voxuanquynhlien@gmail.com>
2019-08-10 01:34:59 +0700

    config

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => 'https://db4free.net/phpMyAdmin/',
+	'username' => 'quynhlien',
+	'password' => 'classtmdt',
 	'database' => 'tocotoco',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
