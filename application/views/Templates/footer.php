eae6918752570d8a200fb49c89b47851b780f5d7
QuynhLien <voxuanquynhlien@gmail.com>
2019-08-13 20:10:33 +0700

    mesenger

--- application/views/Templates/footer.php
+++ application/views/Templates/footer.php
@@ -54,9 +54,27 @@
 <script async src="https://platform-api.sharethis.com/js/sharethis.js#property=5d52ae7f4cd0540012f20160&product="sticky-share-buttons"></script>
 <?php } ?>
 
-<!-- Your customer chat code -->
-<div class="fb-customerchat"
-  attribution=setup_tool
-  page_id="1621330407923772"
+<!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v4.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your customer chat code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="111183013571051"
   theme_color="#d3b673">
-</div>
+      </div>
